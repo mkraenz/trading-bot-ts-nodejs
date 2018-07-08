591470e074cef845610081498e22d88a6fe5f7fc
Mirco Kraenz <prosingularity@yahoo.de>
2018-07-08 21:54:36 +0900

    #3 ADD several libraries

--- ts-trading-bot/app/settings.ts
+++ ts-trading-bot/app/settings.ts
@@ -5,19 +5,28 @@ export let dbPricesCollection = "prices";
 
 
 // PricesFromCsv and others
-export const PATH_TO__PRICES = '../data/pricesGOOG.csv';
+//export const PATH_TO__PRICES = '../data/EOAN.DE.csv';
+//
+// many
+//export const STOCK_SYMBOL = "EOAN.DE";
 
+// PricesFromCsv and others
+export const PATH_TO__PRICES = '../data/GOOG.csv';
+//
 // many
 export const STOCK_SYMBOL = "GOOG";
 
 
+
+
 // bot
-export const PERIODS = 504; // # entries in pricesGOOG.csv
-export const ROI = 1.5; // sell if ROI threshold is reached
-export const AMOUNT = 50 // buy and sell batches of 50 stocks
+//export const PERIODS = 504; // # entries in GOOG.csv
+export const PERIODS = 256; // # entries in EOAN.DE.csv 
+export const ROI = 1.3; // sell if ROI threshold is reached
+export const AMOUNT = 6000 // buy and sell batches of AMOUNT stocks
 
 
 // broker
 export const INITIAL_STOCKS = 0;
-export const FEE = 50;
+export const FEE = 2.5;
 export const INITIAL_CASH = 100000;
